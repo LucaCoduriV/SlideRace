9c418d57f9286ff3a60917b9447767ea9def8a7a
Luca Coduri <coduri.vitor@gmail.com>
2020-05-25 23:24:16 +0200

    Ajout du launcher

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Ch.Luca.MyGame
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+        #region Public Fields
+
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        [SerializeField]
+        private GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField]
+        private GameObject progressLabel;
+
+        #endregion
+
+        #region Private Serializable Fields
+
+        [SerializeField]
+        private byte maxPlayersPerRoom = 4;
+
+        #endregion
+
+        #region Private Fields
+
+        /// <summary>
+        /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+        /// </summary>
+        string gameversion = "1";
+
+        /// <summary>
+        /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon,
+        /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
+        /// Typically this is used for the OnConnectedToMaster() callback.
+        /// </summary>
+        bool isConnecting;
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        void Awake()
+        {
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Start the connection process.
+        /// - If already connected, we attempt joining a random room
+        /// - if not yet connected, Connect this application instance to Photon Cloud Network
+        /// </summary>
+        public void Connect()
+        {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                isConnecting = PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameversion;
+            }
+        }
+
+        #endregion
+
+        #region MonoBehaviourPunCallbacks Callbacks
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+            if (isConnecting)
+            {
+                PhotonNetwork.JoinRandomRoom();
+                isConnecting = false;
+            }
+            
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+            isConnecting = false;
+
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+            
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+            // #Critical: We only load if we are the first player, else we rely on `PhotonNetwork.AutomaticallySyncScene` to sync our instance scene.
+            if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                //load the room
+                PhotonNetwork.LoadLevel("Room for 1");
+            }
+
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Ch.Luca.MyGame
+{
+    /// <summary>
+    /// Player name input field. Let the user input his name, will appear above the player in the game.
+    /// </summary>
+    [RequireComponent(typeof(InputField))]
+    public class PlayerNameInputField : MonoBehaviour
+    {
+
+        #region Private Constants
+        const string playerNamePrefKey = "PlayerName";
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            string defaultName = string.Empty;
+            InputField _inputField = this.GetComponent<InputField>();
+            if (_inputField != null)
+            {
+                if (PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    _inputField.text = defaultName;
+                }
+            }
+
+            PhotonNetwork.NickName = defaultName;
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void SetPlayerName(string value)
+        {
+            // #important
+            if (string.IsNullOrEmpty(value))
+            {
+                Debug.LogError("Player Name is null or empty");
+                return;
+            }
+            PhotonNetwork.NickName = value;
+
+            PlayerPrefs.SetString(playerNamePrefKey, value);
+        }
+
+        #endregion
+
+
+    }
+}
+
